2aadc8772c4ef236edf6d3c7c34b49d81b3eabac
devchas <devchas@gmail.com>
2016-10-14 15:39:35 -0400

    cleanup API calls and custom fields

--- server/controllers/contact_list_controller.js
+++ server/controllers/contact_list_controller.js
@@ -3,38 +3,24 @@ const path = require('path');
 const Settings = require('../../settings');
 const optIn = 'opt-in';
 
-function prepareEmail(emailBody) {
-
-	console.log(emailBody);
-
-	const senderEmail = Settings.senderEmail;
-	const senderName = Settings.senderName;
-	const recipientEmail = emailBody.email;
-	const firstName = emailBody.firstName;
-	const lastName = emailBody.lastName;
-	const favoriteColor = emailBody.favoriteColor;
+function prepareEmail(reqBody) {
 	const subject = "Please Confirm Your Email Address";
 	const url = Settings.url + '/success';
 	const link = "<a href='" + url + "'>this link</a>"
 	const mailText = "Thanks for signing up! Click " + link + " to sign up!  This link will be active for 24 hours.";
-	const timeSent = String(Date.now());
-	const templateId = Settings.templateId;
 
 	var emailBody = {
 	  personalizations: [
 	    {
 	      to: [
 	        {
-	          email: recipientEmail,
+	          email: reqBody.email,
 	        }
 	      ],
 	      subject: subject,
 	      custom_args: {
-	      	firstName: firstName,
-	      	lastName: lastName,
-	      	favoriteColor: favoriteColor,
 	      	type: optIn,
-	      	time_sent: timeSent,
+	      	time_sent: String(Date.now()),
 	      },
 	      substitutions: {
 	      	link_insert: link
@@ -42,8 +28,8 @@ function prepareEmail(emailBody) {
 	    },
 	  ],
 	  from: {
-	    email: senderEmail,
-	    name: senderName,
+	    email: Settings.senderEmail,
+	    name: Settings.senderEmail,
 	  },
 	  content: [
 	    {
@@ -53,44 +39,37 @@ function prepareEmail(emailBody) {
 	  ],
 	}
 
+	const templateId = Settings.templateId;
 	if (templateId) emailBody.template_id = templateId;
+
+	for (key in reqBody) {
+		emailBody.personalizations[0].custom_args[key] = reqBody[key];
+	}
+
 	return emailBody;
-}
-
-function send(toSend) {
-	return new Promise(function(resolve, request) {
-		var requestBody = toSend
-		var emptyRequest = require('sendgrid-rest').request
-		var requestPost = JSON.parse(JSON.stringify(emptyRequest))
-		var isEmailSent;
-		requestPost.method = 'POST'
-		requestPost.path = '/v3/mail/send'
-		requestPost.body = requestBody
-		
-		sg.API(requestPost, function (error, response) {
-	    	console.log(response.statusCode)
-	    	console.log(response.body)
-	    	console.log(response.headers)
-
-			if (response.statusCode >= 200 && response.statusCode < 300) {
-				resolve(true);
-			} else {
-				resolve(false);
-			}
-		});
-	});
 }
 
 // Send confirmation email to contact with link to confirm email
 exports.sendConfirmation = (req, res, next) => {
-	send(prepareEmail(req.body)).then(function(response) {
-		if (response) {
+	var request = sg.emptyRequest({
+		method: 'POST',
+		path: '/v3/mail/send',
+		body: prepareEmail(req.body)
+	});
+
+	sg.API(request, function(error, response) {
+		if (error) {
+			console.log('Error response received');
+		}
+		console.log(response.statusCode);
+		console.log(response.body);
+		console.log(response.headers);
+
+		if (response.statusCode >= 200 && response.statusCode < 300) {
 			res.sendFile(path.join(__dirname, '../static/check-inbox.html'));
 		} else {
 			res.sendFile(path.join(__dirname, '../static/error.html'));
 		}
-	}, function(err) {
-		console.log(err);
 	});
 }
 
@@ -102,6 +81,19 @@ exports.addUser = function(req, res, next) {
 }
 
 function addUserToList(emailBody, callback) {
+	console.log(emailBody);
+
+	var ignoreFields = ['ip', 'sg_event_id', 'sg_message_id', 'useragent', 'event',
+		'url_offset', 'time_sent', 'timestamp', 'url', 'type'];
+
+	var customFields = [{}];
+
+	for (key in emailBody) {
+		if (!stringInArray(key, ignoreFields)) {
+			customFields[0][key] = emailBody[key];
+		}
+	}
+	
 	const email = emailBody.email;
 	const firstName = emailBody.firstName;
 	const lastName = emailBody.lastName;
@@ -114,44 +106,46 @@ function addUserToList(emailBody, callback) {
 
 	// Confirm email type is opt in and link has been clicked within 1 day
 	if (emailType == optIn && timeElapsed < secondsInDay) {
-		// Create new contact
-		const requestBody = [{ 
-			email: email,
-			first_name: firstName,
-			last_name: lastName,
-			favorite_color: favoriteColor
-		}];
+		var request = sg.emptyRequest({
+			method: 'POST',
+			path: '/v3/contactdb/recipients',
+			body: customFields
+		});
 
-		var emptyRequest = require('sendgrid-rest').request
-		var requestPost = JSON.parse(JSON.stringify(emptyRequest))
-		requestPost.method = 'POST'
-		requestPost.path = '/v3/contactdb/recipients'
-		requestPost.body = requestBody
-		sg.API(requestPost, function (error, response) {
+		sg.API(request, function (error, response) {
 	    	console.log(response.statusCode)
 	    	console.log(response.body)
 	    	console.log(response.headers)
 
 	    	if (listId) {
-				// Add contact to list
 				var contactID = JSON.parse(response.body.toString()).persisted_recipients[0];			
-				var emptyRequest = require('sendgrid-rest').request
-				var requestPost = JSON.parse(JSON.stringify(emptyRequest))
-				requestPost.method = 'POST'
-				requestPost.path = '/v3/contactdb/lists/' + listId + '/recipients/' + contactID;
-				sg.API(requestPost, function (error, response) {
+				var request = sg.emptyRequest({
+					method: 'POST',
+					path: '/v3/contactdb/lists/' + listId + '/recipients/' + contactID,
+					body: customFields
+				});
+				sg.API(request, function (error, response) {
 			    	console.log(response.statusCode)
 			    	console.log(response.body)
 			    	console.log(response.headers)
 					
 					callback();
 				});
-	    	} else {
-				callback();	    		
-	    	}
-
+			} else {
+				callback();
+			}
 		});
 	} else {
 		callback();
 	}
+}
+
+function stringInArray(string, array) {
+	var isInArray = false;
+	array.map((item) => {
+		if (string == item) {
+			isInArray = true;
+		}
+	});
+	return isInArray;
 }
