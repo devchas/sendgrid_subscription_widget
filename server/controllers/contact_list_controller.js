483763c54fdaf2de672a2fc3d242e282aa6e0a4c
devchas <devchas@gmail.com>
2016-10-17 17:18:52 -0400

    auto add nonexistant fields

--- server/controllers/contact_list_controller.js
+++ server/controllers/contact_list_controller.js
@@ -87,53 +87,111 @@ function addUserToList(emailBody, callback) {
 		'url_offset', 'time_sent', 'timestamp', 'url', 'type'];
 
 	var customFields = [{}];
+	var customFieldArr = [];
 
 	for (key in emailBody) {
 		if (!stringInArray(key, ignoreFields)) {
 			customFields[0][key] = emailBody[key];
+			if (key != 'email' && key != 'first_name' && key != 'last_name') {
+				customFieldArr.push(key);
+			}
 		}
 	}
-	
-	const emailType = emailBody.type;
-	const timestamp = parseInt(emailBody.time_sent);
-	const listId = Settings.listId;
-	const secondsInDay = 86400;
-	const timeElapsed = (Date.now() - timestamp) / 1000;
 
-	// Confirm email type is opt in and link has been clicked within 1 day
-	if (emailType == optIn && timeElapsed < secondsInDay) {
-		var request = sg.emptyRequest({
-			method: 'POST',
-			path: '/v3/contactdb/recipients',
-			body: customFields
+	checkAndAddCustomFields(customFieldArr, function() {
+		const emailType = emailBody.type;
+		const timestamp = parseInt(emailBody.time_sent);
+		const listId = Settings.listId;
+		const secondsInDay = 86400;
+		const timeElapsed = (Date.now() - timestamp) / 1000;
+
+		// Confirm email type is opt in and link has been clicked within 1 day
+		if (emailType == optIn && timeElapsed < secondsInDay) {
+			var request = sg.emptyRequest({
+				method: 'POST',
+				path: '/v3/contactdb/recipients',
+				body: customFields
+			});
+
+			sg.API(request, function(error, response) {
+		    	console.log(response.statusCode)
+		    	console.log(response.body)
+		    	console.log(response.headers)
+
+		    	if (listId) {
+					var contactID = JSON.parse(response.body.toString()).persisted_recipients[0];			
+					var request = sg.emptyRequest({
+						method: 'POST',
+						path: '/v3/contactdb/lists/' + listId + '/recipients/' + contactID,
+						body: customFields
+					});
+					sg.API(request, function(error, response) {
+				    	console.log(response.statusCode)
+				    	console.log(response.body)
+				    	console.log(response.headers)
+						
+						callback();
+					});
+				} else {
+					callback();
+				}
+			});
+		} else {
+			callback();
+		}
+	});
+	
+}
+
+function checkAndAddCustomFields(submittedFields, callback) {
+	var request = sg.emptyRequest({
+		method: 'GET',
+		path: '/v3/contactdb/custom_fields',
+	});
+
+	sg.API(request, function(error, response) {
+    	console.log(response.statusCode)
+    	console.log(response.body)
+    	console.log(response.headers)
+
+    	var existingCustomFields = JSON.parse(response.body);
+		var fieldsToCreate = [];
+		
+		submittedFields.map((submittedField) => {
+			var fieldExists = false;
+			existingCustomFields.custom_fields.map((field) => {
+				if (submittedField == field.name) {
+					fieldExists = true;
+				}
+			});
+			if (!fieldExists) {
+				fieldsToCreate.push(submittedField)
+			}
 		});
 
-		sg.API(request, function (error, response) {
-	    	console.log(response.statusCode)
-	    	console.log(response.body)
-	    	console.log(response.headers)
+		if (fieldsToCreate.length == 0) {
+			callback();
+		} else {
+			fieldsToCreate.map((fieldsToCreate) => {
+				var body = { name: fieldsToCreate, type: 'text' };
 
-	    	if (listId) {
-				var contactID = JSON.parse(response.body.toString()).persisted_recipients[0];			
 				var request = sg.emptyRequest({
 					method: 'POST',
-					path: '/v3/contactdb/lists/' + listId + '/recipients/' + contactID,
-					body: customFields
+					path: '/v3/contactdb/custom_fields',
+					body: body
 				});
-				sg.API(request, function (error, response) {
+
+				sg.API(request, function(error, response) {
 			    	console.log(response.statusCode)
 			    	console.log(response.body)
 			    	console.log(response.headers)
-					
-					callback();
-				});
-			} else {
-				callback();
-			}
-		});
-	} else {
-		callback();
-	}
+
+			    	callback();
+			    });
+			});
+		}
+
+    });
 }
 
 function stringInArray(string, array) {
