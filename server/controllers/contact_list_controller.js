4f9abe6b8d3d694453ef2123e5475325003f9a18
Brandon West <brawest@gmail.com>
2017-02-06 14:14:58 -0700

    Add support for notification emails

--- server/controllers/contact_list_controller.js
+++ server/controllers/contact_list_controller.js
@@ -5,11 +5,10 @@ const path = require('path');
 const Settings = require('../../settings');
 const optIn = 'opt-in';
 
-function prepareEmail(reqBody) {
-	const subject = "Please Confirm Your Email Address";
+function prepareNotificationEmail(reqBody) {
+	const subject = "New signup "
 	const url = formatUrl(Settings.url) + '/success';
-	const link = "<a href='" + url + "'>this link</a>"
-	const mailText = "Thanks for signing up! Click " + link + " to sign up!  This link will be active for 24 hours.";
+	const mailText = "Thanks for signing up. <a href='" + url + "'>Please click here to confirm your email address.</a>  This link will be active for 24 hours.";
 
 	var emailBody = {
 	  personalizations: [
@@ -23,9 +22,6 @@ function prepareEmail(reqBody) {
 	      custom_args: {
 	      	type: optIn,
 	      	time_sent: String(Date.now()),
-	      },
-	      substitutions: {
-	      	link_insert: link
 	      }
 	    },
 	  ],
@@ -51,6 +47,78 @@ function prepareEmail(reqBody) {
 	return emailBody;
 }
 
+function prepareEmail(reqBody) {
+	const subject = reqBody.first_name;
+	const url = formatUrl(Settings.url) + '/success';
+	const mailText = "Thanks for signing up. <a href='" + url + "'>Please click here to confirm your email address.</a>  This link will be active for 24 hours.";
+
+	var emailBody = {
+	  personalizations: [
+	    {
+	      to: [
+	        {
+	          email: reqBody.email,
+	        }
+	      ],
+	      subject: subject,
+	      custom_args: {
+	      	type: optIn,
+	      	time_sent: String(Date.now()),
+	      }
+	    },
+	  ],
+	  from: {
+	    email: Settings.senderEmail,
+	    name: Settings.senderEmail,
+	  },
+	  content: [
+	    {
+	      type: "text/html",
+	      value: mailText,
+	    }
+	  ],
+	}
+
+	const templateId = Settings.templateId;
+	if (templateId) emailBody.template_id = templateId;
+
+	for (key in reqBody) {
+		emailBody.personalizations[0].custom_args[key] = reqBody[key];
+	}
+
+	return emailBody;
+}
+
+function prepareNotificationEmail(reqBody) {
+	const subject = "New email signup";
+	const mailText = "A new person just confirmed via your email subscription widget.<br/>" + reqBody.first_name + " " + reqBody.last_name + "<br/>" + reqBody.email;
+
+	var emailBody = {
+	  personalizations: [
+	    {
+	      to: [
+	        {
+	          email: Settings.notificationEmail,
+	        }
+	      ],
+	      subject: subject
+	    },
+	  ],
+	  from: {
+	    email: Settings.senderEmail,
+	    name: Settings.senderEmail,
+	  },
+	  content: [
+	    {
+	      type: "text/html",
+	      value: mailText,
+	    }
+	  ],
+	}
+
+	return emailBody;
+}
+
 // Send confirmation email to contact with link to confirm email
 exports.sendConfirmation = (req, res, next) => {
 	var request = sg.emptyRequest({
@@ -63,9 +131,6 @@ exports.sendConfirmation = (req, res, next) => {
 		if (error) {
 			console.log('Error response received');
 		}
-		console.log(response.statusCode);
-		console.log(response.body);
-		console.log(response.headers);
 
 		if (response.statusCode >= 200 && response.statusCode < 300) {
 			res.sendFile(path.join(__dirname, '../static/check-inbox.html'));
@@ -78,6 +143,23 @@ exports.sendConfirmation = (req, res, next) => {
 // Create new contact and add contact to given list
 exports.addUser = function(req, res, next) {
 	addUserToList(req.body[0], function() {
+		//send notification about the new signup
+		if(Settings.sendNotification) {
+			console.log("Sending notification");
+
+			var request = sg.emptyRequest({
+				method: 'POST',
+				path: '/v3/mail/send',
+				body: prepareNotificationEmail(req.body[0])
+			});
+
+			sg.API(request, function(error, response) {
+				if (error) {
+					console.log('Error sending notification');
+				}
+			});
+		}
+
 		res.sendStatus(200);
 	});
 }
@@ -86,7 +168,7 @@ function addUserToList(emailBody, callback) {
 	console.log(emailBody);
 
 	var ignoreFields = ['ip', 'sg_event_id', 'sg_message_id', 'useragent', 'event',
-		'url_offset', 'time_sent', 'timestamp', 'url', 'type'];
+		'url_offset', 'time_sent', 'timestamp', 'url', 'type', 'smtp-id'];
 
 	var customFields = [{}];
 	var customFieldArr = [];
@@ -116,12 +198,8 @@ function addUserToList(emailBody, callback) {
 			});
 
 			sg.API(request, function(error, response) {
-		    	console.log(response.statusCode)
-		    	console.log(response.body)
-		    	console.log(response.headers)
-
 		    	if (listId) {
-					var contactID = JSON.parse(response.body.toString()).persisted_recipients[0];			
+					var contactID = JSON.parse(response.body.toString()).persisted_recipients[0];
 					var request = sg.emptyRequest({
 						method: 'POST',
 						path: '/v3/contactdb/lists/' + listId + '/recipients/' + contactID,
@@ -131,7 +209,7 @@ function addUserToList(emailBody, callback) {
 				    	console.log(response.statusCode)
 				    	console.log(response.body)
 				    	console.log(response.headers)
-						
+
 						callback();
 					});
 				} else {
@@ -142,7 +220,7 @@ function addUserToList(emailBody, callback) {
 			callback();
 		}
 	});
-	
+
 }
 
 function checkAndAddCustomFields(submittedFields, callback) {
@@ -158,7 +236,7 @@ function checkAndAddCustomFields(submittedFields, callback) {
 
     	var existingCustomFields = JSON.parse(response.body);
 		var fieldsToCreate = [];
-		
+
 		submittedFields.map((submittedField) => {
 			var fieldExists = false;
 			existingCustomFields.custom_fields.map((field) => {
@@ -184,10 +262,6 @@ function checkAndAddCustomFields(submittedFields, callback) {
 				});
 
 				sg.API(request, function(error, response) {
-			    	console.log(response.statusCode)
-			    	console.log(response.body)
-			    	console.log(response.headers)
-
 			    	callback();
 			    });
 			});
