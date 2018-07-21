fd46d8c15f81cacb4792e0cb0bc3d328076ba264
Yura <jura@tuta.io>
2018-07-21 18:59:40 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 // Change the url to the domain of your app
-exports.url = '';
+exports.url = 'http://localhost:3090';
 
-exports.senderEmail = '';
-exports.senderName = '';
+exports.senderEmail = 'sender@example.com';
+exports.senderName = 'Sender Name';
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
-exports.listId = 2480976;
+exports.listId = null;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = '';
+exports.templateId = null;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
