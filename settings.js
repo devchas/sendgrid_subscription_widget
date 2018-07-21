e5c8f14e4b014f0d5b1cc81df42b4faf04e3e110
Yura <jura@tuta.io>
2018-07-21 18:53:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // Change the url to the domain of your app
-exports.url = 'https://gulfex.herokuapp.com';
+exports.url = '';
 
-exports.senderEmail = 'yura@gulfex.net';
-exports.senderName = 'Gulfex';
+exports.senderEmail = '';
+exports.senderName = '';
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
@@ -10,8 +10,8 @@ exports.listId = 2480976;
 
 // set 'exports.templateId = null' to opt out of using a template
 // or a string with the templateId to use a template
-exports.templateId = 'e439b612-53e0-40f1-8cbf-1ab31649623e';
+exports.templateId = '';
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = 'yura@gulfex.net';
+exports.notificationEmail = '';
