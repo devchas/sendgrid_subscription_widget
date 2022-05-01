0165ef4438d7d6de7d344afb68807e8d4d0d6bea
Yasuhito Suganuma <yasuhito.suganuma@gmail.com>
2022-05-01 23:37:07 +0900

    Update settings.

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://sgsubscriptionwidgeta.herokuapp.com/';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "yasuhito.suganuma@gmail.com";
+exports.senderName = "Yasuhito Suganuma";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
